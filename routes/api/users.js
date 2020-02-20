0aa9c534fdcfc9bcfaca3751cdf9aecb7cae53de
Charles Cifuentes <charlescifuentes@gmail.com>
2020-02-20 11:35:30 -0500

    Creating the User Registration route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
 
@@ -36,11 +38,28 @@ router.post(
       }
 
       // Get users gravatar
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
 
       // Encrypt password
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
 
       // Return jsonwebtoken
-      res.send('User Route');
+      res.send('User registered');
     } catch (err) {
       console.error(err.message);
       res.status('500').send('Server error');
