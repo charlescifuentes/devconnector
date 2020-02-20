fb916b830915fbeabb580b907ed9801afd7dc2cc
Charles Cifuentes <charlescifuentes@gmail.com>
2020-02-20 10:12:32 -0500

    Configuring the Users Route and Model

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,51 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
 
-// @route GET api/users
-// @desc Test route
+const User = require('../../models/User');
+
+// @route POST api/users
+// @desc  Register User
 // @acces Public
-router.get('/', (req, res) => res.send('User Route'));
+router.post(
+  '/',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characteres'
+    ).isLength({ min: 6 })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-module.exports = router
+    const { name, email, password } = req.body;
+
+    try {
+      // See if user exists
+      let user = await User.findOne({ email });
+
+      if (User) {
+        res.status(400).json({ errors: [{ msg: 'User already exist' }] });
+      }
+
+      // Get users gravatar
+
+      // Encrypt password
+
+      // Return jsonwebtoken
+      res.send('User Route');
+    } catch (err) {
+      console.error(err.message);
+      res.status('500').send('Server error');
+    }
+  }
+);
+
+module.exports = router;
