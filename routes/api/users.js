0e189f3df4f6510463dc42559803b20ba2492ac8
Charles Cifuentes <charlescifuentes@gmail.com>
2020-02-21 00:08:33 -0500

    Adding a JWT to Users Route

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
@@ -33,8 +35,8 @@ router.post(
       // See if user exists
       let user = await User.findOne({ email });
 
-      if (User) {
-        res.status(400).json({ errors: [{ msg: 'User already exist' }] });
+      if (user) {
+        return res.status(400).json({ errors: [{ msg: 'User already exist' }] });
       }
 
       // Get users gravatar
@@ -58,8 +60,21 @@ router.post(
 
       await user.save();
 
-      // Return jsonwebtoken
-      res.send('User registered');
+      const payload = {
+        user: {
+        id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload, 
+        config.get('jwtSecret'),
+        { expiresIn: 36000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });  
+        }
+      ); 
     } catch (err) {
       console.error(err.message);
       res.status('500').send('Server error');
